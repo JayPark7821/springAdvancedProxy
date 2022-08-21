fd13c68ec048bc3f8ab7d880bbedc91060191298
JayPark7821 <pjhyun1990@gmail.com>
2022-08-22 07:58:42 +0900

    프록시 팩토리 예제 2

--- src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
+++ src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
@@ -1,5 +1,6 @@
 package hello.proxy.proxyfactory;
 
+import hello.proxy.common.service.ConcreteService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,11 +31,42 @@ public class ProxyFactoryTest {
 		Assertions.assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
 		Assertions.assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
 
-
-		//given
-
-	    //when
-
-	    //then
 	}
+	@Test
+		@DisplayName("구체 클래스만 있으면 CGLIB 사용")
+	void concreteProxy() throws Exception{
+		ConcreteService target = new ConcreteService();
+		ProxyFactory proxyFactory = new ProxyFactory(target);
+		proxyFactory.addAdvice(new TimeAdvice());
+		ConcreteService proxy = (ConcreteService)proxyFactory.getProxy();
+		log.info("targetClass = {} ", target.getClass());
+		log.info("proxyClass = {} ", proxy.getClass());
+
+		proxy.call();
+
+		Assertions.assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+		Assertions.assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+		Assertions.assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+
+	}
+
+	@Test
+	@DisplayName("ProxyTargetClass옵션을 사용하면 인터페이스가 있어도 GCLIB를 사용하고 클래스 기반 프록시 사용")
+	void proxyTargetClass() throws Exception{
+		ServiceInterface target = new ServiceImpl();
+		ProxyFactory proxyFactory = new ProxyFactory(target);
+		proxyFactory.setProxyTargetClass(true);
+		proxyFactory.addAdvice(new TimeAdvice());
+		ServiceInterface proxy = (ServiceInterface)proxyFactory.getProxy();
+		log.info("targetClass = {} ", target.getClass());
+		log.info("proxyClass = {} ", proxy.getClass());
+
+		proxy.save();
+
+		Assertions.assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+		Assertions.assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+		Assertions.assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+
+	}
+
 }
